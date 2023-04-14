5bcafa193a713f22a79ed8ce1a1d90c60d918592
dvlproad <dvlproad@163.com>
2023-04-15 03:39:02 +0800

    【Feature】helloworld

--- helloworld.sh
+++ helloworld.sh
@@ -2,13 +2,18 @@
 
 echo "hello world"
 
-echo "1.1、创建 https://github.com/dvlpCI/script-branch-json-file.git，并编写提交脚本代码"
+echo "1.1、创建 https://github.com/dvlpCI/script-branch-json-file.git"
 
-echo "1.2、在上述创建的git下编写提交脚本代码并打tag到远程"
+echo "1.2、在上述创建的git下创建 helloworld.sh，并编写想要的脚本代码"
 
-echo "1.3、假如上述打的tag是0.0.1，则可以直接在浏览器里输入 https://github.com/dvlpCI/script-branch-json-file/archive/0.0.1.tar.gz 来回车下来该文件"
+echo "1.3.1、shc -r -f helloworld.sh"
+echo "1.3.2、修改上述生成的二进制可执行文件helloworld.sh.x名为helloworld(不填的话，你后续使用时候就不能输helloworld，而是要输helloworld.sh)"
 
-echo "1.4、shasum -a 256 0.0.1.tar.gz"
+echo "1.4、提交文件并打tag到远程"
+
+echo "1.5、假如上述打的tag是0.0.1，则可以直接在浏览器里输入 https://github.com/dvlpCI/script-branch-json-file/archive/0.0.1.tar.gz 来回车下来该文件"
+
+echo "1.6、shasum -a 256 0.0.1.tar.gz"
 
 echo "hello world"
 
