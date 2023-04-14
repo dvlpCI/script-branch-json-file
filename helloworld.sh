c6bce292a47ddb6b7277cc66db4f93777ac38caa
dvlproad <dvlproad@163.com>
2023-04-15 03:14:04 +0800

    【Feature】helloworld

---
+++ helloworld.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "hello world"
+
+echo "1.1、创建 https://github.com/dvlpCI/script-branch-json-file.git，并编写提交脚本代码"
+
+echo "1.2、在上述创建的git下编写提交脚本代码并打tag到远程"
+
+echo "1.3、假如上述打的tag是0.0.1，则可以直接在浏览器里输入 https://github.com/dvlpCI/script-branch-json-file/archive/0.0.1.tar.gz 来回车下来该文件"
+
+echo "1.4、shasum -a 256 0.0.1.tar.gz"
+
+echo "hello world"
+
+echo "hello world"
+
+echo "2.1、创建 https://github.com/dvlpCI/homebrew-branch-json-file.git"
+
+echo "2.2、在上述创建的git下添加 helloworld.rb 文件及其内容"
+
+echo "hello world"
+
+echo "hello world"
+
+echo "tar -czvf v0.0.2.tar.gz helloworld.sh"
+
+echo "mv v0.0.2.tar.gz archive/"
+
+echo "hello world"
+
+echo "hello world"
+
+echo "brew tap dvlpCI/homebrew-branch-json-file"
+
+echo "hello world"
+
+echo "hello world"
