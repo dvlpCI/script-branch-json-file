8b9ec5dab567fac7a257c8fba018b774d6e30263
dvlproad <dvlproad@163.com>
2023-05-05 21:39:55 +0800

    【Update】优化提示

--- src/lastBranchJsonFile_update.py
+++ src/lastBranchJsonFile_update.py
@@ -1,9 +1,9 @@
 '''
 Author: dvlproad dvlproad@163.com
 Date: 2023-04-12 22:15:22
-LastEditors: dvlproad dvlproad@163.com
-LastEditTime: 2023-04-19 01:21:51
-FilePath: /branchJsonFile_create/branchInfoManager.py
+LastEditors: dvlproad
+LastEditTime: 2023-05-05 21:38:45
+FilePath: lastBranchJsonFile_update.py
 Description: 分支Json文件的信息更新
 '''
 # -*- coding: utf-8 -*-
@@ -29,7 +29,10 @@ def choose_last_branchs_info_file_path():
     # 获取上级目录，使用 os.path.abspath()函数获取jenkins_workspace文件夹的绝对路径，能有效去除目录路径结尾可能多一个/的问题
     jenkins_workspace_dir_path=os.path.abspath(jenkins_workspace_dir_path)
     last_branchs_info_dir_path = os.path.dirname(jenkins_workspace_dir_path)
-    # print("文件路径2：", last_branchs_info_dir_path)
+    if not os.path.isdir(last_branchs_info_dir_path):
+        print(f"{RED}目录{last_branchs_info_dir_path}不存在，请检查{NC}")
+        return 1
+    
     
     # 获取testDir文件夹下所有JSON文件的路径
     # 在这行代码中，f是一个变量名，用于迭代os.listdir(path)函数返回的文件名列表。os.listdir(path)返回指定目录下的所有文件和文件夹的名称列表，然后通过列表推导式[os.path.join(path, f) for f in os.listdir(path) if f.endswith('.json')]筛选出以.json结尾的文件，并使用os.path.join()函数将它们的路径与目录合并。在这个列表推导式中，f代表os.listdir(path)返回的列表中的每个文件名。
@@ -47,7 +50,7 @@ def choose_last_branchs_info_file_path():
             exit(2)
             break
         elif not re.match(r'^[a-zA-Z0-9_]+\.[jJ][sS][oO][nN]$', user_input):
-            print(f"{RED}输入的不是JSON文件名，请重新输入{NC}")
+            print(f"{RED}输入的{user_input}不是JSON文件名，请重新输入{NC}")
             continue
         elif user_input not in [os.path.basename(file) for file in lastBranchsInfo_files]:
             print(f"{RED}文件不存在，请重新输入{NC}")
@@ -81,6 +84,8 @@ def removeJsonByName(file_path, removeBranchName):
 # 请选择操作类型
 def removeJsonByInputName(): 
     last_branchs_info_file_path=choose_last_branchs_info_file_path()
+    if last_branchs_info_file_path == 1:
+        return 1
     print("您等下将在以下文件里删除分支名：\033[1;31m{}\033[0m\n".format(last_branchs_info_file_path))
     
     print("文件中的分支名列表：")
