3e1fa72860747637b5b2fa3588e5355548b88521
dvlproad <dvlproad@163.com>
2023-05-09 15:11:10 +0800

    【Update】增加提示

--- src/lastBranchJsonFile_update.py
+++ src/lastBranchJsonFile_update.py
@@ -40,6 +40,10 @@ def choose_last_branchs_info_file_path():
     # 在这行代码中，f是一个变量名，用于迭代os.listdir(path)函数返回的文件名列表。os.listdir(path)返回指定目录下的所有文件和文件夹的名称列表，然后通过列表推导式[os.path.join(path, f) for f in os.listdir(path) if f.endswith('.json')]筛选出以.json结尾的文件，并使用os.path.join()函数将它们的路径与目录合并。在这个列表推导式中，f代表os.listdir(path)返回的列表中的每个文件名。
     lastBranchsInfo_files = [joinFullPath(last_branchs_info_dir_path, f) for f in os.listdir(last_branchs_info_dir_path) if f.endswith('.json')]
 
+    if lastBranchsInfo_files.__len__ == 0:
+        print(f"{RED}目录last_branchs_info_dir_path={last_branchs_info_dir_path}下未找到json文件，请检查{NC}")
+        return 1
+
     # 打印文件列表
     print("文件列表：")
     for i, file in enumerate(lastBranchsInfo_files):
