604d5edabc3f49299df8cec44cc5581c71e63459
dvlproad <dvlproad@163.com>
2024-04-04 04:35:41 +0800

    【Feature】iOS 重签名 resign_ipa

---
+++ sign/get_entitlements_plist_file_from_embedded_mobileprovision_file.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+# 从 embedded.mobileprovision 获取生成签名需要的 entitlements.plist
+# 原理：将 Entitlements_plist_file 文件中 从 <key>Entitlements</key> 行开始到 <key>ExpirationDate</key> 之间的每一行添加到新文件中
+# <key>Entitlements</key>
+# 	<dict>
+		
+# 				<key>application-identifier</key>
+# 		<string>8285LLMDV3.com.dvlproad.TSDemoDemo</string>
+				
+# 				<key>keychain-access-groups</key>
+# 		<array>
+# 				<string>8285LLMDV3.*</string>
+# 		</array>
+				
+# 				<key>get-task-allow</key>
+# 		<true/>
+				
+# 				<key>com.apple.developer.team-identifier</key>
+# 		<string>8285LLMDV3</string>
+
+# 	</dict>
+# 	<key>ExpirationDate</key>
+
+
+# 使用方法:
+
+# shell 参数具名化
+while [ -n "$1" ]
+do
+    case "$1" in
+        # 新证书授权文件(可以从本地可以真机安装的APP，中通过显示包内容来看到)
+        -embedded_mobileprovision_F|--embedded-mobileprovision-file-path) Embedded_MOBILEPROVISION_PATH=$2; shift 2;; # 本分支【当前打包】的所有分支名数组字符串
+        # 通过 embedded.mobileprovision 得到的 entitlements.plist 所希望存放的路径
+        -entitlements_plist_hope_path|--entitlements_plist_file_path) Entitlements_plist_file=$2; shift 2;; # 本分支【当前打包】的所获得的所有分支名数组是从哪个时间点开始获取来的
+        --) break ;;
+        *) break ;;
+    esac
+done
+
+
+# 定义颜色常量
+NC='\033[0m' # No Color
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+
+
+function debug_log() {
+	echo "$1" >&2  # 使用>&2将echo输出重定向到标准错误，作为日志
+}
+
+
+
+# 将 mobileprovision 转换为 plist 格式
+embedded_is_in_directory=$(dirname "$Embedded_MOBILEPROVISION_PATH")
+embedded_plist_file="${embedded_is_in_directory}/embedded.plist"
+security cms -D -i "${Embedded_MOBILEPROVISION_PATH}" > "$embedded_plist_file"
+# open $embedded_plist_file
+
+
+# 使用PlistBuddy创建plist文件，并添加键值对
+if [ -f "Entitlements_plist_file" ]; then
+    rm Entitlements_plist_file
+fi
+
+# 从源 embedded_plist_file 文件中提取指定字段的值
+# key1="AppIDName"
+# value1=$(/usr/libexec/PlistBuddy -c "Print :$key1" "$embedded_plist_file")
+# echo "===== 字符串: $value1"
+# /usr/libexec/PlistBuddy -c "Add :$key1 string $value1" "$Entitlements_plist_file"
+
+
+# # 创建 plist 文件并添加键值对
+# cat << EOF > "$Entitlements_plist_file"
+# <?xml version="1.0" encoding="UTF-8"?>
+# <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+# <plist version="1.0">
+# <dict>
+#     <key>key1</key>
+#     <string>value1</string>
+# </dict>
+# </plist>
+# EOF
+# open $Entitlements_plist_file
+# exit 1
+
+
+# 创建 plist 文件，及添加签名的行
+debug_log "${GREEN}======创建 Entitlements 的 plist 文件，及添加前面的行======${NC}"
+cat << EOF > "$Entitlements_plist_file"
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+EOF
+
+
+debug_log "${GREEN}======为 Entitlements 的 plist 文件添加真正的内容的行======${NC}"
+start_pattern="<key>Entitlements</key>"
+end_pattern="<key>ExpirationDate</key>"
+line_num=1
+canUse=false
+while read -r line; do
+    if [[ $line == *"${start_pattern}"* ]]; then
+        # echo "$line_num $line"
+        # echo "这是开头的标记"
+        canUse=true
+        
+    elif [[ $line == *"${end_pattern}"* ]]; then
+        # echo "$line_num $line"
+        # echo "这是结尾的标记"
+        canUse=false
+
+    else
+        if $canUse; then
+            debug_log "$line_num $line"
+            echo "$line" >> "$Entitlements_plist_file"    # 在文件末尾添加一行
+        fi
+    fi
+    line_num=$((line_num+1))
+done < $embedded_plist_file
+
+debug_log "${GREEN}======为 Entitlements 的 plist 文件添加尾部的行======${NC}"
+echo "</plist>" >> "$Entitlements_plist_file"    # 在文件末尾添加一行
+# open $Entitlements_plist_file
--- sign/resign_ipa.sh
+++ sign/resign_ipa.sh
@@ -13,6 +13,25 @@
 
 #终端执行：直接把脚本export_resign_app.sh 拖到终端 回车执行
 
+
+# 当前【shell脚本】的工作目录
+CurrentDIR_Script_Absolute="$( cd "$( dirname "$0" )" && pwd )"
+
+# 定义颜色常量
+NC='\033[0m' # No Color
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+
+
+function debug_log() {
+	echo "$1" >&2  # 使用>&2将echo输出重定向到标准错误，作为日志
+}
+
+
 # # 获取当前脚本所在目录
 # script_dir="$(cd "$(dirname "$0")" && pwd)"
 # # 工程根目录
@@ -31,6 +50,7 @@ echo "\n\033[32m+++++++++++++++++开始：项目路径+++++++++++++++++\033[0m"
 # SRCROOT="$script_dir"
 # #导出APP路径
 # BUILT_PRODUCTS_DIR="${SRCROOT}/Payload"
+BUILT_PRODUCTS_DIR="/Users/qian/Documents/重签名/Payload"
 # #APP文件命名
 # TARGET_NAME="NewAPP"
 
@@ -38,32 +58,35 @@ echo "\n\033[32m+++++++++++++++++开始：项目路径+++++++++++++++++\033[0m"
 # 证书名或者证书上SHA-1(显示的字符串，不需求空格),二者都可以
 # EXPANDED_CODE_SIGN_IDENTITY="Apple Development: xxx (S8****VL)"
 # EXPANDED_CODE_SIGN_IDENTITY="A2AB7AF90****8C7D889E"
-EXPANDED_CODE_SIGN_IDENTITY="Apple Development: chaoqian li (L4V8UVH3CZ)"
+# EXPANDED_CODE_SIGN_IDENTITY="Apple Development: chaoqian li (L4V8UVH3CZ)"
+EXPANDED_CODE_SIGN_IDENTITY="8CFD7D009A854B698A90AB2D17B8D71F0867857D"
+# 新证书授权文件(可以从本地可以真机安装的APP，中通过显示包内容来看到)
+RESIGN_NEED_MOBILEPROVISION_PATH="/Users/qian/Documents/重签名/TSDemoDemo.app/embedded.mobileprovision"
+
+
+embedded_is_in_directory=$(dirname "$RESIGN_NEED_MOBILEPROVISION_PATH")
+
+# 从 embedded.mobileprovision 获取生成签名需要的 entitlements.plist
+# RE_SIGN_NEW_ENTITLEMENST="$SRCROOT/new_entitlements.plist"
+RE_SIGN_NEW_ENTITLEMENST="/Users/qian/Documents/重签名/plist_from_mp.plist"
+sh "$CurrentDIR_Script_Absolute/get_entitlements_plist_file_from_embedded_mobileprovision_file.sh" \
+    -embedded_mobileprovision_F "$RESIGN_NEED_MOBILEPROVISION_PATH" \
+    -entitlements_plist_hope_path "$RE_SIGN_NEW_ENTITLEMENST"
+# open $RE_SIGN_NEW_ENTITLEMENST
+
+# 从源 plist 文件中提取 bundle_id 字段的值
+team_bundle_id=$(/usr/libexec/PlistBuddy -c "Print :application-identifier" "$RE_SIGN_NEW_ENTITLEMENST")
+PRODUCT_BUNDLE_IDENTIFIER="${team_bundle_id#*.}"    # 截取第一个.号后的文本
+debug_log "${GREEN}bundle_id = $PRODUCT_BUNDLE_IDENTIFIER${NC}"
 
 #bundle_id
 PRODUCT_BUNDLE_IDENTIFIER="com.dvlproad.TSDemoDemo"
-# 新证书授权文件(本地可以安装的APP，显示包内容
-# TARGET_MOBILEPROVISION_PATH="$SRCROOT/test.mobileprovision"
-TARGET_MOBILEPROVISION_PATH="/Users/qian/Documents/重签名/TSDemoDemo.app/embedded.mobileprovision"
-# #直接把上面文件重命名，用于后续签名（后面是重新复制命名）
-# ORIGIN_MOBILEPROVISION_PATH="$SRCROOT/embedded.mobileprovision"
-ORIGIN_MOBILEPROVISION_PATH="/Users/qian/Documents/重签名/TSOverlayDemo.app/embedded.mobileprovision"
 
-# #手动创建修改的plist
-# RE_SIGN_NEW_ENTITLEMENST="$SRCROOT/new_entitlements.plist"
-RE_SIGN_NEW_ENTITLEMENST="/Users/qian/Documents/重签名/plist_from_mp.plist"
-
-# #手动解析证书授权文件的关键信息，
-# NEW_PROVISION="$SRCROOT/new_provision.plist"
 
 # echo "\n\033[32m=======1、自动 获取正确的配置文件+++++++++++++++++\033[0m"
 
-# 将新证书授权文件覆盖旧的【embedded.mobileprovision】
-cp "$TARGET_MOBILEPROVISION_PATH" "$ORIGIN_MOBILEPROVISION_PATH"
-# #上面用手动的
 
-# security cms -D -i "$ORIGIN_MOBILEPROVISION_PATH" >"$NEW_PROVISION"
-# /usr/libexec/PlistBuddy -x -c 'Print :Entitlements' "$NEW_PROVISION" | tee "$RE_SIGN_NEW_ENTITLEMENST"
+# #上面用手动的
 
 # echo "\n\033[32m========2、解压IPA 到Temp,(解压出来的.app拷贝Payload)路径+++++++++++++++++\033[0m"
 
@@ -108,9 +131,11 @@ echo "app路径:$TARGET_APP_PATH"
 # echo "\033[32m========3、info.plist+++++++++++++++++\033[0m"
 
 # #----------------------------------------
-# # 4. 更新info.plist文件 CFBundleIdentifier
-# #  设置:"Set : KEY Value" "目标文件路径"
-/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $PRODUCT_BUNDLE_IDENTIFIER" "$TARGET_APP_PATH/Info.plist"
+# 将新证书授权文件覆盖旧的【embedded.mobileprovision】
+cp "$RESIGN_NEED_MOBILEPROVISION_PATH" "${TARGET_APP_PATH}/embedded.mobileprovision"
+# 4. 更新info.plist文件 CFBundleIdentifier
+#  设置:"Set : KEY Value" "目标文件路径"
+/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $PRODUCT_BUNDLE_IDENTIFIER" "${TARGET_APP_PATH}/Info.plist"
 # /usr/libexec/PlistBuddy -c "Set :CFBundleName $TARGET_NAME" "$TARGET_APP_PATH/Info.plist"
 # #----------------------------------------
 
@@ -126,12 +151,12 @@ TARGET_APP_FRAMEWORKS_PATH="$TARGET_APP_PATH/Frameworks"
 if [ -d "$TARGET_APP_FRAMEWORKS_PATH" ]; then
     for FRAMEWORK in "$TARGET_APP_FRAMEWORKS_PATH/"*; do
 
-        # 三方重签名
+        # 三方重签名:   codesign -fs "（证书名称）" （FrameWork名称）
         /usr/bin/codesign --force --sign "$EXPANDED_CODE_SIGN_IDENTITY" "$FRAMEWORK"
     done
 fi
 
-/usr/bin/codesign -fs "$EXPANDED_CODE_SIGN_IDENTITY" --entitlements="$RE_SIGN_NEW_ENTITLEMENST" "$TARGET_APP_PATH"
+/usr/bin/codesign -fs "$EXPANDED_CODE_SIGN_IDENTITY" --no-strict --entitlements="$RE_SIGN_NEW_ENTITLEMENST" "$TARGET_APP_PATH"
 
 # #----------------------------------------
 # # 7.压缩包
